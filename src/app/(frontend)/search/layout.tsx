c35bcddc97fa82cf2bc1f9a743ab6b7df8ee6b73
Ocherkashchenko <oleh.cherkashchenko@keabank.com>
2025-04-14 13:49:28 +0200

    refactor: wrap Search component in Suspense for improved loading handling

--- src/app/(frontend)/search/layout.tsx
+++ src/app/(frontend)/search/layout.tsx
@@ -2,13 +2,15 @@ import { Categories } from '@/components/layout/search/Categories'
 import { FilterList } from '@/components/layout/search/filter'
 import { sorting } from '@/lib/constants'
 import { Search } from '@/components/Header/Search'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export default function SearchLayout({ children }: { children: React.ReactNode }) {
   return (
     <React.Fragment>
       <div className="container flex flex-col gap-8 my-16 pb-4 ">
-        <Search />
+        <Suspense fallback={null}>
+          <Search />
+        </Suspense>
         <div className="w-full flex-none">
           <Categories />
           <FilterList list={sorting} title="Sort by" />
